c1dca0f572ab1f301bcbd228fedd5dc04e3b4bb5
Ritu Bafna <ritujane78@gmail.com>
2026-06-08 11:59:01 +0545

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# JobPortal (in progress)
+
+This is a Spring Boot 4 project that contains profile dashboards of the job issuer and candidate, with Thymeleaf used for frontend.
---
+++ src/test/java/com/jane/JobportalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jane;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobportalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
